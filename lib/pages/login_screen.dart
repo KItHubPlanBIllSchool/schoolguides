ac529ef5ead4f7e3151f58d895926176f95fef6b
AWDW <47055316+KItHubPlanBIllSchool@users.noreply.github.com>
2023-10-13 10:49:44 +0900

    modified:   .dart_tool/flutter_build/534bf234f299b3ee2d0c241119913fe7/.filecache 	modified:   .dart_tool/flutter_build/534bf234f299b3ee2d0c241119913fe7/app.dill 	modified:   .dart_tool/flutter_build/534bf234f299b3ee2d0c241119913fe7/debug_android_application.stamp 	modified:   .dart_tool/flutter_build/534bf234f299b3ee2d0c241119913fe7/flutter_assets.d 	modified:   .dart_tool/flutter_build/534bf234f299b3ee2d0c241119913fe7/kernel_snapshot.d 	modified:   .dart_tool/flutter_build/534bf234f299b3ee2d0c241119913fe7/kernel_snapshot.stamp 	modified:   .flutter-plugins-dependencies 	modified:   lib/pages/login_screen.dart

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
 
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hackathon/data/colors.dart';
 import 'package:hackathon/scripts/snack_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -71,131 +73,168 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        leading: BackButton(
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(Color.fromRGBO(0, 0, 0, 1))),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
       resizeToAvoidBottomInset: false,
       body: Padding(
-        padding: const EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(30.10),
         child: Form(
           key: formKey,
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
-                child: Center(
-                    child: Text(
-                  'FURshop',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      fontFamily: 'montserrat'),
-                )),
+              const SizedBox(
+                height: 100,
+              ),
+              SvgPicture.asset(
+                'lib/assets/images/Vector.svg',
+                height: 75,
               ),
               const Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 90, 0, 80),
-                child: Center(
-                    child: Text(
-                  'Войдите\nпо электронной почте',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      height: 1.3,
-                      fontFamily: 'montserrat'),
-                  textAlign: TextAlign.center,
-                )),
+                padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 15),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Вход',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'onest',
+                        fontWeight: FontWeight.w800),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                controller: emailTextInputController,
-                validator: (email) =>
-                    email != null && !EmailValidator.validate(email)
-                        ? 'Введите правильный Email'
-                        : null,
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsetsDirectional.fromSTEB(50, 7, 0, 0),
-                  hintStyle: TextStyle(
-                      color: Color.fromRGBO(162, 162, 162, 1),
-                      fontSize: 16,
-                      fontFamily: 'montserrat'),
-                  hintText: 'Введите эл.почту',
+              const Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 15),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Введите данные своего аккаунта',
+                    style: TextStyle(
+                        color: const Color.fromRGBO(164, 172, 183, 1),
+                        fontSize: 15),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(153, 216, 216, 216),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextFormField(
+                  style: TextStyle(color: textColor),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  controller: emailTextInputController,
+                  validator: (email) =>
+                      email != null && !EmailValidator.validate(email)
+                          ? 'Введите правильный Email'
+                          : null,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsetsDirectional.fromSTEB(15, 2, 0, 0),
+                    hintStyle: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 0.573),
+                        fontSize: 16,
+                        fontFamily: 'onest'),
+                    hintText: 'Введите эл.почту',
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
-              TextFormField(
-                autocorrect: false,
-                controller: passwordTextInputController,
-                obscureText: isHiddenPassword,
-                validator: (value) => value != null && value.length < 6
-                    ? 'Минимум 6 символов'
-                    : null,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  contentPadding:
-                      const EdgeInsetsDirectional.fromSTEB(50, 0, 0, 0),
-                  hintStyle: const TextStyle(
-                      color: Color.fromRGBO(162, 162, 162, 1),
-                      fontSize: 16,
-                      fontFamily: 'montserrat'),
-                  hintText: 'Пароль',
-                  suffix: GestureDetector(
-                    onTap: togglePasswordView,
-                    child: Icon(
-                      isHiddenPassword
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                      color: Colors.black,
+              Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(153, 216, 216, 216),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextFormField(
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                  autocorrect: false,
+                  controller: passwordTextInputController,
+                  obscureText: isHiddenPassword,
+                  validator: (value) => value != null && value.length < 6
+                      ? 'Минимум 6 символов'
+                      : null,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding:
+                        const EdgeInsetsDirectional.fromSTEB(15, -10, 10, -5),
+                    hintStyle: const TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontSize: 16,
+                        fontFamily: 'onest'),
+                    hintText: 'Пароль',
+                    suffix: GestureDetector(
+                      onTap: togglePasswordView,
+                      child: Icon(
+                        isHiddenPassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color: textColor,
+                      ),
                     ),
                   ),
                 ),
               ),
               const SizedBox(height: 30),
-              ElevatedButton(
-                style: ButtonStyle(
-                    elevation: MaterialStateProperty.all(5),
+              Container(
+                height: 60.0,
+                width: 1001,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromRGBO(64, 123, 255, 1)),
                     shape: MaterialStateProperty.all(
                       RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(70),
                       ),
                     ),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white)),
-                onPressed: login,
-                child: const SizedBox(
-                    height: 34,
-                    width: 221,
-                    child: Center(
-                        child: Text(
-                      'Войти',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'montserrat'),
-                    ))),
+                  ),
+                  onPressed: login,
+                  child: const SizedBox(
+                      height: 34,
+                      width: 221,
+                      child: Center(
+                          child: Text(
+                        'Войти',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'onest'),
+                      ))),
+                ),
               ),
               const SizedBox(height: 20),
               GestureDetector(
                 onTap: () => Navigator.of(context).pushNamed('/signup'),
                 child: Text(
-                  'Регистрация',
+                  'Нет аккаунта? Регистрация',
                   style: TextStyle(
-                      color: Color.fromRGBO(162, 162, 162, 1),
-                      fontFamily: 'montserrat',
+                      color: Color.fromRGBO(176, 182, 189, 1),
+                      fontFamily: 'comfortaa',
                       fontWeight: FontWeight.normal,
                       fontSize: 14),
                 ),
               ),
               const SizedBox(height: 15),
-              GestureDetector(
-                onTap: () => Navigator.of(context).pushNamed('/reset_password'),
-                child: Text(
-                  'Сбросить пароль',
-                  style: TextStyle(
-                      color: Color.fromRGBO(162, 162, 162, 1),
-                      fontFamily: 'montserrat',
-                      fontWeight: FontWeight.normal,
-                      fontSize: 14),
-                ),
-              ),
             ],
           ),
         ),
